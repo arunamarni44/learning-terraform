e68f55dd560538c856b55262efa50dbb3883a099
arunamarni44 <157057440+arunamarni44@users.noreply.github.com>
2024-02-10 15:32:48 -0500

    Update create-bucket.tf

--- create-bucket.tf
+++ create-bucket.tf
@@ -1,14 +1,11 @@
 resource "aws_s3_bucket" "a" {
 
   bucket = var.bucket_name
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-  }
- "b" {
+  } 
   bucket = "my-test-bucket-456"
-
   tags = {
     Name        = "My-Bucket-b"
     Environment = "Dev"
